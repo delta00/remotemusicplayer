c1c02cff871777ed853afe9dadb6191312f85787
tomas.zima.66 <tomas.zima.66@c0f23793-9d2e-78da-47fb-4a31fe1ffa4f>
2013-09-08 21:28:05 +0000

    Preparation of the controller.

--- RemoteMusicPlayerServer/src/application/Main.java
+++ RemoteMusicPlayerServer/src/application/Main.java
@@ -1,10 +1,103 @@
 package application;
 
-import maryb.player.Player;
+import java.io.IOException;
+
+import tools.communicator.Communicator;
+import tools.communicator.ConnectionDescriptor;
+import tools.communicator.ConnectionListener;
+import tools.communicator.PlayerState;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		Player player = new Player();
+		final Communicator communicator = new Communicator();
+		
+		communicator.setConnectionListener(new ConnectionListener() {
+			@Override
+			public String update() {
+				System.out.println("Update.");
+				return "<musicLibrary></musicLibrary>";
+			}
+			
+			@Override
+			public boolean stop() {
+				System.out.println("Stop.");
+				return true;
+			}
+			
+			@Override
+			public void setActiveConnection(ConnectionDescriptor connectionDescriptor) {
+				System.out.printf("Aktivni pripojeni: %s\n", connectionDescriptor);
+			}
+			
+			@Override
+			public boolean play(String filename) {
+				System.out.printf("Hrat: %s\n", filename);
+				return true;
+			}
+			
+			@Override
+			public boolean pause() {
+				System.out.println("Pause.");
+				return true;
+			}
+			
+			@Override
+			public PlayerState getState() {
+				System.out.println("Get state");
+				return new PlayerState(false, "Sybreed", "Slave design", "Bioactive", 320, 60);
+			}
+			
+			@Override
+			public boolean checkVersion(long version) {
+				System.out.printf("check %d\n", version);
+				return false;
+			}
+			
+			@Override
+			public boolean authenticate(String device, String password) {
+				System.out.printf("autorizace: \"%s\", \"%s\"\n", device, password);
+				return false;
+			}
+
+			@Override
+			public void close() {
+				System.out.println("spojeni uzavreno");
+			}
+
+			@Override
+			public void invalidCommand() {
+				System.out.println("Invalid command");
+			}
+		});
+		
+		Thread communicatorThread = new Thread() {
+			@Override
+			public void run() {
+				try {
+					communicator.run();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		};
+		
+		communicatorThread.start();
+		
+/*		System.out.println("MAIN: Cekam...");
+		Thread.currentThread().sleep(15000);
+		
+		System.out.println("MAIN: Uz se, kurva, chystam to ukoncit.");
+		
+		while (communicator.hasOpenedConnection()) {
+			Thread.currentThread().sleep(100);
+		}
+		
+		System.out.println("MAIN: Ukoncuju to, kurva...");
+		communicatorThread.interrupt();*/
+		
+		// Player usage
+/*		Player player = new Player();
 		
 		player.setSourceLocation("/home/tom/Music/01-krucipusk-druide-mcz.mp3");
 		player.setCurrentVolume(1.0f);
@@ -16,8 +109,9 @@ public class Main {
 		
 		Thread.currentThread().sleep(2000);
 		
-		player.play();
+		player.play();*/
 		
+		// MusicLibrary usage
 /*		long t0 = System.currentTimeMillis();
 		
 		MusicLibrary musicLibrary = new MusicLibrary();
---
+++ RemoteMusicPlayerServer/src/application/controller/Controller.java
@@ -0,0 +1,13 @@
+package application.controller;
+
+import tools.MusicLibrary;
+import tools.communicator.Communicator;
+
+public class Controller {
+	private Communicator	communicator	= new Communicator(9999);
+	private MusicLibrary	musicLibrary	= new MusicLibrary();
+	
+	public void run() {
+		//
+	}
+}
